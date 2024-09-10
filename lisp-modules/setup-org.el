11e9d32328885870a2ada83b5f1d81958106d06a
niraj <niraj.apr4@gmail.com>
2024-09-10 18:18:08 +0545

    minor mode : write-config-mode written and latex-fragments is enlarged

--- lisp-modules/setup-org.el
+++ lisp-modules/setup-org.el
@@ -18,4 +18,32 @@
  'org-babel-load-languages
  '((julia . t)))
 
+
+(define-minor-mode my/writing-config-mode
+  "A minor mode that enables a collection of other minor modes"
+  :init-value nil
+  :lighter " Write-Config"
+  :keymap (make-sparse-keymap)
+  (if my/writing-config-mode
+      (progn
+        ;; Enable desired minor modes
+	(flyspell-mode 1)
+        (auto-fill-mode 1)
+	(setq-local fill-column 72)
+	(org-indent-mode 1)
+	(org-preview-mode 1)
+	(yas-minor-mode 1)
+        )
+    ;; Disable the minor modes when turning off 
+    (flyspell-mode -1)
+    (auto-fill-mode -1)
+    (org-indent-mode -1)
+    (org-auctex-mode -1)
+    (yas-minor-mode -1)
+    ;; Disable any other minor modes you enabled
+    ))
+
+;; Optionally, you can add a key binding to toggle the mode
+(global-set-key (kbd "C-c w") 'my/writing-config-mode)
+
 (provide 'setup-org)
