da5d6fc0b6e543008aad46c4bdcbc424ac221a93
niraj <niraj.apr4@gmail.com>
2024-10-28 16:32:29 +0545

    treesit support added for rust

--- lisp/setup-prog-modes.el
+++ lisp/setup-prog-modes.el
@@ -4,6 +4,19 @@
 (add-hook 'prog-mode-hook #'display-line-numbers-mode)
 (add-hook 'prog-mode-hook #'hs-minor-mode)
 
+;; ---------------- treesit -----------------------
+(use-package treesit
+  :config
+  (setq treesit-language-source-alist
+        '((rust "https://github.com/tree-sitter/tree-sitter-rust")
+	  (bash "https://github.com/tree-sitter/tree-sitter-bash")))
+  ;; Auto-install rust grammar if it's missing
+  (unless (treesit-language-available-p 'rust)
+    (treesit-install-language-grammar 'rust))
+  (unless (treesit-language-available-p 'bash)
+    (treesit-install-language-grammar 'bash)))
+;; -------------------------------------------------
+
 ;; ------------- julia editor support --------------
 
 (use-package julia-mode
@@ -21,8 +34,14 @@
 ;; -------------------------------------------------
 
 ;; ---------------- rust support -------------------
+(use-package rust-mode
+  :ensure t
+  :init
+  (setq rust-mode-treesitter-derive t))
+
 (use-package rustic
   :ensure t
+  :after(rust-mode)
   :config
   (setq rustic-lsp-client 'eglot)
   (setq rustic-format-on-save nil)
