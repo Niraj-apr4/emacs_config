ceaa7372e689da07da78f2b6dc3d64ee301bc258
niraj <niraj.apr4@gmail.com>
2025-05-13 18:03:09 +0545

    elpaca version fix

--- init.el
+++ init.el
@@ -2,7 +2,7 @@
 ;; *    PACKAGE MANAGEMENT 
 ;; ** ELPACA
 
-(defvar elpaca-installer-version 0.7)
+(defvar elpaca-installer-version 0.11)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
